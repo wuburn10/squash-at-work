60340b86ff8ebe912e5454744d05d04e695b2776
Reuben Lee Suppiah <reuben.leesuppiah@intellogic.com.my>
2025-10-28 22:52:02 +0800

    refactor: Remove Base Path at Vite Config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/squash-at-work",
+  // base: "/squash-at-work",
 })
